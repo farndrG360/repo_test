fcc036e4a2edf2552969ef5111413b624d26b0f5
farnoosh naderi <farnooshnaderi@aol.com>
2020-12-03 17:09:38 +0200

    first step

---
+++ test_1.py
@@ -0,0 +1,9 @@
+print('hello world')
+print('Vision\nAcademy')
+print('Vision\tAcademy')
+print('Vision\'Academy\'')
+print()
+print('train with feri')
+print()
+print('Vision\nAcademy\t')
+
---
+++ test_2.py
@@ -0,0 +1,69 @@
+print('train2')
+c= 23
+e='23'
+b = str(c) + '22'
+b_ = int(e) + 22
+b_ = b_+1
+print(b_ == 30)
+#print(b)
+print(b_)
+print(b_+1)
+print(b_)
+b__ = 12.35
+print(b__)
+b__ = b__*25
+print(b__)
+#b__= str (b__)
+b__= b__ / 25
+print(b__)
+
+aa= 8.5
+print(aa + b__)
+print(b__*25 / aa)
+aa= 33
+fe= '25'
+g= str(aa) + '25'
+gg= int(fe) + 33
+print(gg + 24.8 )
+print('gg')
+print(gg <=20)
+
+name = input('please insert your name: ')
+team = input('what is your favorite soccor team? ')
+print ('hello'  '! + your favorite team is + team)
+
+num1 = input ('please insert the first number: ')
+num2 = input ('please insert the second number: ')
+operation = input ('please insert your desire operation among: ')
+if operation == '+':
+   results = num1 + num2
+if operation == '-': 
+    result = num1 - num2
+if operation == '*':
+    result = num1 * num2
+if operation == '/':
+    result = num1 / num2
+    
+print(result)            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
